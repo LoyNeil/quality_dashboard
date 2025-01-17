3eac0cd1dc1d1d6301b386cf522da26721cda975
Loy Neil <loyneil92@gmail.com>
2025-01-17 17:34:29 +0530

    URL Updated - Cors

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const port = 5000;
 
 // Enable CORS for React frontend
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // React app URL
+  res.header("Access-Control-Allow-Origin", "https://qualitydashboard.netlify.app"); // React app URL
   res.header("Access-Control-Allow-Methods", "GET");
   res.header("Access-Control-Allow-Headers", "Content-Type");
   next();
