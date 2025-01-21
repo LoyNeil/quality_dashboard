6e6c32e42ed72457fd6e1705c59ae7561e7992a0
Loy Neil <loyneil92@gmail.com>
2025-01-21 18:25:38 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import { NavBar } from './Navbar';
+import { LoadingProfile } from './LoaderContext.js';
 
 export default function App() {
   return (
-    <div>
-      <h1>Quality Dashboard</h1>
-    </div>
+      <div>
+        <LoadingProfile>
+          <h1>Quality Dashboard</h1>
+          <NavBar />
+        </LoadingProfile>
+      </div>
   );
 }
