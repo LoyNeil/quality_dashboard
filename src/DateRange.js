959223e7fc16da832992deba4b5af8b37e2b0ed1
Loy Neil <loyneil92@gmail.com>
2025-01-21 18:25:55 +0530

    Update DateRange.js

--- src/DateRange.js
+++ src/DateRange.js
@@ -16,29 +16,33 @@ export function DateeRange({onDateRangeChange}) {
   };
 
   // State to manage selected date range
-  const [selectedDate, setSelectedDate] = useState([null]);
+  const [selectedDate, setSelectedDate] = useState([dayjs().subtract(1, 'days'), dayjs().subtract(1, 'days')]);
 
   // Handle Calendar Change
   const handleCalenderClick = (dates) => {
     if (dates && dates.length === 2) {
       const [start, end] = dates;
       if (start && end) {
+        if (selectedDate && start.isSame(selectedDate[0]) && end.isSame(selectedDate[1])) {
+          console.log("No change in date range, skipping update.");
+          return;
+        }
         // Calculate difference in months
         const monthsDifference = end.diff(start, 'months', true);
         if (monthsDifference > 3) {
           message.error('Date range cannot be greater than 3 months.');
-          setSelectedDate(null);
+          setSelectedDate([null, null]);
         } else {
           setSelectedDate(dates);
           onDateRangeChange(dates);
         }
       } else {
-        setSelectedDate(null);
-        onDateRangeChange(null);
+        setSelectedDate([null, null]);
+        onDateRangeChange([null, null]);
       }
     } else {
-      setSelectedDate(null);
-      onDateRangeChange(null);
+      setSelectedDate([null,null]);
+      onDateRangeChange([null,null]);
     }
   };
 
@@ -50,7 +54,7 @@ export function DateeRange({onDateRangeChange}) {
         disabledDate={disabledDate}
         value={selectedDate}
         className="custom-range-picker"
-        onCalendarChange={handleCalenderClick}
+        onChange={handleCalenderClick}
       />
     </Space>
   );
