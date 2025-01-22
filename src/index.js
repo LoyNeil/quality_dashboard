039407def87edf3157e67edf8f14030f00bc388a
Loy Neil <loyneil92@gmail.com>
2025-01-22 10:39:28 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,15 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { NavBar } from './Navbar';
-import { LoadingProvider } from './ContextLoader';
+import { LoadingProfile } from './LoaderContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <LoadingProvider>
+  <LoadingProfile>
     <App />
-    <NavBar />
-    </LoadingProvider>
-  </React.StrictMode>
+  </LoadingProfile>
 );
